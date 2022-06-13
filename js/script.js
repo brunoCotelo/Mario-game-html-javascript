8ba3bf4aa049f817c6a24f89140a875155174230
Bruno Cotelo <bruno_cotelo@hotmail.com>
2022-06-13 19:08:54 -0300

    Acerto da colisao

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const mario = document.getElementById("mario");
+const pipe = document.getElementById("pipe");
 
 const jump = () =>{
+    document.removeEventListener('keydown', jump);
     mario.classList.add('jump');
-
+    console.log("altura do mario: " + mario.offsetTop);
     setTimeout(()=>{
         mario.classList.remove('jump');
+        document.addEventListener('keydown', jump);
     }, 500);
 }
 
@@ -16,8 +18,7 @@ const loop = setInterval(() => {
     if(pipePosition <= 120){
        
         pipe.style.animation = 'none';
-        
-       pipe.style.left = '${pipePosition}px';
+        pipe.style.left = `${pipePosition}px`;
    
     }
 
